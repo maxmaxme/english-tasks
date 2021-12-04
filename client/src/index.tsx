15e2ae404d13c00ffb19ea51efa4888fd803c30f
Maxim Lepekha <m.lepekha@yandex.ru>
2021-12-04 20:48:19 +0530

    Deploy frontend to VK Mini Apps

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,6 @@
 import { AdaptivityProvider } from '@vkontakte/vkui';
 import React from 'react';
 import { render } from 'react-dom';
-import bridge from '@vkontakte/vk-bridge';
 import { App } from './components/App';
 import AppContextProvider from './providers/AppContextProvider';
 import AppConfigProvider from './providers/AppConfigProvider';
