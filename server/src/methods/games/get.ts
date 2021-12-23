8bf7022aadef4e03b57a62cf3a085084163364f2
Maxim Lepekha <m.lepekha@yandex.ru>
2021-12-24 00:33:47 +0530

    Two new number games

--- server/src/methods/games/get.ts
+++ server/src/methods/games/get.ts
@@ -1,7 +1,8 @@
 import atOnIn from '../../games/atOnIn';
 import { pastSimple, v3 } from '../../games/irregularVerbs';
+import { numberToText, soundToText } from '../../games/numbers';
 import { top1000wordsQuiz, top1000wordsQuizTranslate } from '../../games/top1000words';
-import { GameId, Game } from '../../shared/types/game';
+import { Game, GameId } from '../../shared/types/game';
 
 export const getGames = (): Promise<{[key: GameId]: Game}> => {
   return Promise.resolve({
@@ -11,5 +12,7 @@ export const getGames = (): Promise<{[key: GameId]: Game}> => {
     // 4: { ...top1000wordsInput, id: 4 },
     5: { ...top1000wordsQuiz, id: 5 },
     6: { ...top1000wordsQuizTranslate, id: 6 },
+    7: { ...numberToText, id: 7 },
+    8: { ...soundToText, id: 8 },
   });
 };
