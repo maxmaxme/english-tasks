8410408047d8a0a99fb03a4d7beb9517fb9c2610
Maxim Lepekha <m.lepekha@yandex.ru>
2021-12-24 09:59:49 +0530

    Sound component loader

--- client/src/components/Game/Sound/index.tsx
+++ client/src/components/Game/Sound/index.tsx
@@ -1,5 +1,6 @@
-import { Icon16PlayCircleFillAzure, Icon16PlayCircleFillGray, Icon20PlayCircle, Icon20PlayCircleFillSteelGray } from '@vkontakte/icons';
 import React, { FC, RefObject, useEffect, useMemo, useState } from 'react';
+import { Icon16PlayCircleFillAzure, Icon16PlayCircleFillGray, Icon20PlayCircle, Icon20PlayCircleFillSteelGray } from '@vkontakte/icons';
+import { Spinner } from '@vkontakte/vkui';
 import styles from './index.css';
 import { Icon16PlayCircleFillAzureProps } from '@vkontakte/icons/dist/16/play_circle_fill_azure';
 
@@ -24,11 +25,19 @@ const playIconComponents: { [key: string]: { paused: ComponentType, playing: Com
 export const Sound = ({ url, size, itemRef }: Props) => {
   const audio = useMemo(() => new Audio(url), [url]);
   const [playing, setPlaying] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const toggle = () => setPlaying(!playing);
 
   useEffect(() => {
-    playing ? audio.play() : audio.pause();
+    if (playing) {
+      setLoading(true);
+      audio.play()
+        .catch(() => setPlaying(false))
+        .finally(() => setLoading(false));
+    } else {
+      audio.pause();
+    }
   }, [playing]);
 
   useEffect(() => {
@@ -37,6 +46,10 @@ export const Sound = ({ url, size, itemRef }: Props) => {
       audio.removeEventListener('ended', () => setPlaying(false));
     };
   }, [audio]);
+
+  if (loading) {
+    return <Spinner size="small" />;
+  }
 
   const Component = playIconComponents[size][playing ? 'playing' : 'paused'];
   return <Component getRootRef={itemRef} className={styles.icon} onClick={toggle} />;
